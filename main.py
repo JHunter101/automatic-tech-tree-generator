94569b7acf508fc8ad2ca187d7ca4f79574cfe39
Jack <jackchen8888@gmail.com>
2023-01-24 02:14:59 +0100

    code cleanup

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
 def name_to_id(name):
+    """Convert name to ID"""
     return ids[names.index(name)]
 
 # Import tech tree data
 df = pd.read_csv('tech_info.csv') 
 df = df.applymap(str)
 
-
-
 # Create Base
-f = graphviz.Digraph(filename = "output/cycle 1.gv", graph_attr={'label': 'Cycle 1 Tech Tree', 
-                                                                 'splines': 'ortho', 
-                                                                 'nodesep': '0.5', 
-                                                                 'ranksep': '0.5', 
-                                                                 'rankdir': 'LR', 
-                                                                 'overlap': 'prism', 
-                                                                 'overlap_scaling': '0.01', 
-                                                                 'ratio': '0.71'
+f = graphviz.Digraph(filename = "output/cycle 1.gv", graph_attr={
+                                                                'label': 'Cycle 1 Tech Tree', 
+                                                                'splines': 'ortho', 
+                                                                'nodesep': '0.5', 
+                                                                'ranksep': '0.5', 
+                                                                'rankdir': 'LR', 
+                                                                'overlap': 'prism', 
+                                                                'overlap_scaling': '0.01', 
+                                                                'ratio': '0.71'
                                                                  })
 
 # Extract Base Information
@@ -36,7 +36,6 @@ names = df['Name'].tolist()
 colors = df['C'].tolist()
 for my_id, my_name, my_color in zip(ids, names, colors):
      f.node(my_id, my_name, color = my_color, shape = 'rounded')
-
 
 # Create Connections
 for index, row in df.iterrows():
@@ -61,6 +60,4 @@ for index, row in df.iterrows():
                 else:
                     f.edge(pID, row['ID'], style="dashed", constraint = row['CS'])
                     
-                    
 f.view()
-
